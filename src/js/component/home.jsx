5de28da7c1bbf72d9197e7c91b73de02c4fb3495
michellebessa <michelleandressa12@gmail.com>
2023-03-29 23:24:54 +0000

    format home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
 
 export const TodoList = () => {
+  const [list, setList] = useState([]);
 
-const [list, setList]=useState([])
+  const [inputValue, setInputValue] = useState("");
 
-const [inputValue, setInputValue]=useState("")
+  const addItemsList = (e) => {
+    e.preventDefault();
+    setList([...list, inputValue]);
+    setInputValue("");
+  };
+  const removeItem = (index) => {
+    let newList = list.filter((item, idx) => idx !== index);
 
-const addItemsList = (e) => {
-	e.preventDefault()
-	setList([...list,inputValue])
-	setInputValue("")
-}
-const removeItem=(index)=>{
-    let newList = list.filter((item,idx)=>idx!==index)
+    setList(newList);
+  };
 
-setList(newList)
-}
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="tittle">Todos</h1>
+        <form onSubmit={addItemsList}>
+          <input
+            type="text"
+            placeholder="Add something to do..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </form>
 
-return (
-	<>
-	<div className="text-center">
-	<h1 className="tittle">Todos</h1>
-	<form onSubmit={addItemsList}>
-    <input type="text" placeholder="Add something to do..." value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
-	</form>
-
-	{list.map((ListItem,index)=>{
-	return <li className="list-group-item" key={index}>
-		 {ListItem} <button onClick={()=>removeItem(index)} ><i className="fas fa-trash"></i></button>
-	</li>
-  })}
-    </div>
-	  </>
-  );}
+        {list.map((ListItem, index) => {
+          return (
+            <li className="list-group-item" key={index}>
+              {ListItem}{" "}
+              <button onClick={() => removeItem(index)}>
+                <i className="fas fa-trash"></i>
+              </button>
+            </li>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default TodoList;
